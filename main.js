256a9f4fd938d33d9b60395ba73fd71d9b8e7906
Anja Weigelt <aweigelt@ufl.edu>
2019-10-27 16:08:10 -0400

    Add temperature immediately to boiling-temp span

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 // It is always helpful to use comments in your code!
+
+// Arrow Function 1
+
+// const boilingTemp = document.getElementById('boiling-temp');
+//
+// const addBoiling = () => {
+//   boilingTemp.innerText = "212";
+// }
+// addBoiling();
+
+const boilingTemp = document.getElementById('boiling-temp');
+
+const addBoiling = () => {
+  const boilingText = document.createTextNode('212');
+  boilingTemp.appendChild(boilingText);
+}
+addBoiling();
+
+// Arrow Function 2
+
+// Arrow Function 3
